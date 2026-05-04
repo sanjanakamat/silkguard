c756a718808a17515d36e1a00b700076b3d99635
sanjana <203809180+SA123nJaNa1234@users.noreply.github.com>
2026-05-04 14:36:22 +0530

    updated training

--- src/pipelines/train.py
+++ src/pipelines/train.py
@@ -33,7 +33,7 @@ def train_model():
 
         # 🔹 Parameters
         params = {
-            "epochs": 2,
+            "epochs": 30,
             "imgsz": 640,
             "batch": 16,
             "patience": 10,
